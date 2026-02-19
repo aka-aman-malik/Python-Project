5948bc8b95d4b9dc82c6b9cf342a965d28bce80a
Muhammad Aman <a12345kashif007@gmail.com>
2026-02-19 21:43:04 +0500

    Delete data3.py

--- data3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df= pd.read_csv('E:/Data analytics/PYTHON/P7/olist_order_payments_dataset.csv/olist_order_payments_dataset.csv')
-
-df = df.drop(columns=['payment_sequential'])
-print(df.head())
-
-df.to_csv('E:/Data analytics/PYTHON/P7/cleaned_order_payments_dataset.csv', index=False)
